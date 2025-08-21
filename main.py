faa02c25a3e12a5960feadcf95da503d5264be49
Svinets <109016419+Svinetc@users.noreply.github.com>
2025-08-21 23:01:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 print('Это основной модуль main.py, его имя в процессе выполнения программы:', __name__)
 
 
-import pack_2
+import pack_3
 import random
 
 print(dir())
 print(dir(pack_2))
 
 pack_2.another_some_func()
+
